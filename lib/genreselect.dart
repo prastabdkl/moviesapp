201617adec33e4eacd985c6758647000eb56e799
prastabdkl <prastabdkl@gmail.com>
2020-12-29 16:02:44 +0545

    implement cast info

---
+++ lib/genreselect.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GenreSelect extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/movieDetailPage.dart
+++ lib/movieDetailPage.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MovieDetail extends StatefulWidget {
   final int movieId;
 
-  MovieDetail({this.movieId}) {
-
-  }
+  MovieDetail({this.movieId});
 
   @override
   _MovieDetailState createState() => _MovieDetailState(movieId: movieId);
@@ -22,6 +21,7 @@ class _MovieDetailState extends State<MovieDetail> {
     _detailPageAPICall(movieId);
   }
   List<Widget> _movieInfo = List();
+  List<Widget> _castList = List();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -34,7 +34,15 @@ class _MovieDetailState extends State<MovieDetail> {
       body: SizedBox(
           // height: height * .7,
           width: width,
-          child: Column(children: _movieInfo)),
+          child:
+              // ListView(children: _movieInfo)),
+              ListView(children: [_movieInfoAndCast()])),
+    );
+  }
+
+  Widget _movieInfoAndCast() {
+    return Column(
+      children: _movieInfo,
     );
   }
 
@@ -72,17 +80,18 @@ class _MovieDetailState extends State<MovieDetail> {
               ),
             ],
           ),
-
+          Divider(),
         ],
       ),
     );
   }
 
   _detailPageAPICall(int movieId) async {
+    // movieId = 15;
     print('going to detail page ');
     // _enableLoading();
     String url =
-        "https://yts.mx/api/v2/movie_details.json?movie_id=$movieId&with_images=false&with_cast=true";
+        "https://yts.mx/api/v2/movie_details.json?movie_id=$movieId&with_images=true&with_cast=true";
     // String url = "https://yts.mx/api/v2/movie_details.json?movie_id=$movieId";
     Response response = await get(url);
     print('done get method ');
@@ -113,6 +122,46 @@ class _MovieDetailState extends State<MovieDetail> {
     Widget movieDetails =
         _detailCard(movieTitle, movieRating, movieRuntime, movieDescription);
     tempList.add(movieDetails);
+    List castInfoList = movieData['cast'];
+    // if(castInfoList)
+    List<Widget> tempList2 = List();
+    if (castInfoList != null) {
+      tempList2.add(Row(
+        children: [
+          Text(
+            movieTitle + '/Cast',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ));
+      for (int i = 0; i < castInfoList.length; i++) {
+        List<Widget> castList = [];
+        for (int i = 0; i < castInfoList.length; i++) {
+          Map eachCastInfoMap = castInfoList[i];
+          String castName = eachCastInfoMap['name'];
+          String imageUrl = eachCastInfoMap['url_small_image'];
+          if (imageUrl == null) {
+            imageUrl =
+                'https://previews.123rf.com/images/pe3check/pe3check1710/pe3check171000054/88673746-no-image-available-sign-internet-web-icon-to-indicate-the-absence-of-image-until-it-will-be-download.jpg';
+          }
+
+          Widget eachcast = _cast(castName, imageUrl);
+          castList.add(eachcast);
+        }
+        //not working horizontal scroll
+        // tempList2.add(ListView(
+        //     scrollDirection: Axis.horizontal,
+        //     children: [Row(children: castList)]));
+        tempList2.add(Row(children: castList));
+        break;
+      }
+    }
+
+    // print(castInfoList);
     List torrentDataList = movieData['torrents'];
     for (int i = 0; i < torrentDataList.length; i++) {
       Map torrentInfoMap = torrentDataList[i];
@@ -122,7 +171,7 @@ class _MovieDetailState extends State<MovieDetail> {
       Widget downloadInfo = _downloadInfo(url, quality, downloadSize);
       tempList.add(downloadInfo);
     }
-    _movieInfo = tempList;
+    _movieInfo = tempList + tempList2;
     setState(() {});
   }
 
@@ -132,10 +181,54 @@ class _MovieDetailState extends State<MovieDetail> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          InkWell(onTap: () => launch(url), child: Text('Download url')),
+          InkWell(
+              onTap: () => launch(url),
+              child: Text(
+                'Download url',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.blue.shade900,
+                    decoration: TextDecoration.underline),
+              )),
           Text(quality),
           Text('Size: $downloadSize')
         ],
+      ),
+    );
+  }
+
+  Widget _cast(String name, String imageUrl) {
+    return Container(
+      height: height * .25,
+      width: width * .22,
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              imageUrl,
+              height: height * .15,
+            ),
+            Text(name),
+
+            // Row(
+            //   children: [Text(title + '/Cast')],
+            // ),
+            // Card(
+            //   child: Container(
+            //     decoration: BoxDecoration(shape: BoxShape.circle),
+            //     child: Column(
+            //       children: [
+            //         // Image.network(imageUrl),
+            //         Text(name),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
